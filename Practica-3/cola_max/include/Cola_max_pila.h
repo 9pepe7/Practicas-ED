5f9d601e22b8b2b656f137696819d9805d31faa2
spiderfan211 <spiderfan211@gmail.com>
2018-11-06 20:31:40 +0100

    Los doxys a falta de revisión

--- Practica-3/cola_max/include/Cola_max_pila.h
+++ Practica-3/cola_max/include/Cola_max_pila.h
@@ -6,7 +6,6 @@
 #ifndef COLA_MAX_PILA_H
 #define COLA_MAX_PILA_H
 #include <stack>
-
 using namespace std;
 
 /**
@@ -24,18 +23,57 @@ using namespace std;
 template <class T>
 class Cola_max{
 private:
+
+/**
+  * @page repColamaxpila Rep del TDA Colamax
+  *
+  */
+
   stack<T> in; /**< Cola para inserciones */
   stack<T> out;/**< Cola para extracciones y consultas*/
   T max; /**< Maximo */
 
 public:
+/**
+  * @brief función push de una cola, mete un elemento en la cola, por detrás de la misma
+  * @param Una variable de tipo T
+  */
   void poner(T x);
+
+/**
+  * @brief función pop de una cola, quita el elemento que primero entró en la misma, el de delante
+  */
   void quitar();
+
+/**
+  * @brief función front de una cola, devuelve el que primero entró en la cola
+  * @return el elemento de tipo T que está en el frente de la cola
+  */
   T frente();
+
+/**
+  * @brief función que comprueba si está vacía una cola
+  * @return una variable booleana que devuelve verdadero si está vacía la cola
+  */
   bool vacia();
+
+/**
+  * @brief función que comprueba el número de elementos actuales de la cola
+  * @return variable entera con el número de elementos de la cola
+  */
   int num_elementos();
+
+/**
+  * @brief función que devuelve el máximo de la cola
+  * @return devuelve una variable de tipo T con el valor máximo de la cola
+  */
   T maximo();
+
 private:
+/**
+  * @brief función que encuentra el máximo de la cola
+  */
   void buscaMaximo();
+
 };
 #endif
--- Practica-3/cola_max/include/Cola_max_vd.h
+++ Practica-3/cola_max/include/Cola_max_vd.h
@@ -12,8 +12,8 @@ using namespace std;
 /**
   * @brief T.D.A. Cola_max
   *
-  * Una instancia del TDA Cola_max es, a efectos practicos, una cola (FIFO) de la que además se puede saber el máximo.
-  * Esta implementación contiene dos vectores, uno con sus miembros y el otro para el maximo.
+  * Una instancia del TDA Cola_max es, a efectos prácticos, una cola (FIFO) de la que además se puede saber el máximo.
+  * Esta implementación contiene dos vectores, uno con sus miembros y el otro para el máximo.
   * El primero actuará como un queue simple con sus miembros en orden.
   * El segundo irá añadiendo los nuevos elementos también, pero además, será modificado si se añade un nuevo máximo.
   *
@@ -21,18 +21,56 @@ using namespace std;
   * @author Emilio Calvo de Mora Mármol
   * @date Noviembre 2018
   */
+
 template <class T>
 class Cola_max{
+
 private:
+
+/**
+  * @page repColamaxvd Rep del TDA Colamax
+  *
+  */
   vector<T> principal; /**< Vector con los elementos*/
+
   vector<T> max;/**< Vector de maximos*/
 
 public:
+
+/**
+  * @brief función push de una cola, mete un elemento en la cola, por detrás de la misma
+  * @param Una variable de tipo T
+  */
   void poner(T x);
+
+/**
+  * @brief función pop de una cola, quita el elemento que primero entró en la misma, el de delante
+  */
   void quitar();
+
+/**
+  * @brief función front de una cola, devuelve el que primero entró en la cola
+  * @return el elemento de tipo T que está en el frente de la cola
+  */
   T frente();
+
+/**
+  * @brief función que comprueba si está vacía una cola
+  * @return una variable booleana que devuelve verdadero si está vacía la cola
+  */
   bool vacia();
+
+/**
+  * @brief función que comprueba el número de elementos actuales de la cola
+  * @return variable entera con el número de elementos de la cola
+  */
   int num_elementos();
+
+/**
+  * @brief función que devuelve el máximo de la cola
+  * @return devuelve una variable de tipo T con el valor máximo de la cola
+  */
   T maximo();
+
 };
 #endif
