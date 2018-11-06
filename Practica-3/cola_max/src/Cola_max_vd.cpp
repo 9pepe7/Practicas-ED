27285747cdcadfd7cc0158825a0c26efce927355
9pepe7 <pepenc97@hotmail.com>
2018-11-06 17:56:04 +0100

    Creo que lo he acabado. El make funciona y da un buen resultado al ejecutarlo

--- Practica-3/cola_max/src/Cola_max_vd.cpp
+++ Practica-3/cola_max/src/Cola_max_vd.cpp
@@ -1,41 +1,42 @@
 #include "Cola_max_vd.h"
 #include <vector>
-#include <iterator>
 
 using namespace std;
 
 
 template <typename T>
-void poner(T x){
+void Cola_max<T>::poner(T x){
   principal.push_back(x);
   max.push_back(x);
-  for(typename vector<T>::iterator it=max.end();it!=max.begin();--it){
-  
+  for(typename vector<T>::iterator it=max.end();it!=max.begin()-1;--it){
+    if(*it > *(it-1)){
+      *(it-1)=*it;
+    }
   }
 }
 
 template <typename T>
-void quitar(){ // pop
+void Cola_max<T>::quitar(){ // pop
   principal.erase(principal.begin());//pop_front
   max.erase(max.begin());
 }
 
 template <typename T>
-T frente(){
+T Cola_max<T>::frente(){ // front
   return principal.front();
 }
 
 template <typename T>
-bool vacia(){
- return true;
+bool Cola_max<T>::vacia(){ // empty
+ return (principal.empty()&&max.empty());
+}
+
+template <typename T> // size
+int Cola_max<T>::num_elementos(){
+ return principal.size();
 }
 
 template <typename T>
-int num_elementos(){
- return 0;
-}
-
-template <typename T>
-T maximo(){
+T Cola_max<T>::maximo(){
   return max.front();
 }
