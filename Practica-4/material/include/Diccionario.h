a535e597d65599a2072bf227f577ccb05d19c154
9pepe7 <pepenc97@hotmail.com>
2018-12-03 18:24:53 +0100

    .h del diccionario

--- Practica-4/material/include/Diccionario.h
+++ Practica-4/material/include/Diccionario.h
@@ -15,15 +15,14 @@ using namespace std;
     *  @brief T.D.A. Diccionario
     *
     * Una instancia del tipo de datos abstracto Diccionario es un objeto
-    * que contiene un array variable de Terminos del T.D.A. Termino, incluido en
-    * este archivo mediante el include "Termino.h", y un valor entero llamado tamanio,
-    * que indica el número de elementos del vector de Terminos.
-    * Lo representamos como una lista con todos los Terminos.
+    * que contiene set de la stl de términos del T.D.A. Termino, incluido en
+    * este archivo mediante el include "Termino.h".
+    * Lo representamos como una lista  ordenada alfabéticamente con todos los Terminos.
     *
     *
-    * @author Emilio Calvo de Mora Mármol
     * @author Jose Manuel Navarro Cuartero
-    * @date Octubre 2018
+    * @author Emilio Calvo de Mora Mármol
+    * @date Diciembre 2018
     */
 
 class Diccionario{
