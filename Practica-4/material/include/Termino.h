baa054ade751cb7f9bca253bcec1cc14f6b139dd
9pepe7 <pepenc97@hotmail.com>
2018-12-03 17:50:51 +0100

    Mas o menos ese es el esqueleto del tda

---
+++ Practica-4/material/include/Termino.h
@@ -0,0 +1,53 @@
+/**
+  * @file Termino.h
+  * @brief Fichero cabecera del TDA Termino
+  *
+  */
+#ifndef TERMINO_H
+#define TERMINO_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/**
+  * @brief T.D.A. Termino
+  *
+  * Una instancia del tipo de datos abstracto Termino es un
+  * objeto que contiene una palabra y todas sus definiciones.
+  * Está compuesto por un par con la palabra en cuestion por un lado, y
+  * un vector que contiene las definiciones por el otro.
+  *
+  * @author José Manuel Navarro Cuartero
+  * @author Emilio Calvo de Mora Mármol
+  * @date Diciembre 2018
+  */
+
+class Termino {
+private:
+  pair<string,vector<string>> termino; /**< Par con la palabra y sus definiciones */
+
+public:
+  Termino();
+  Termino(string pal, vector<string> def);
+  Termino(Termino original);
+
+  string getPalabra();
+  vector<string> getDefiniciones();
+  int getNumDef();
+
+  void setPalabra(string pal);
+  void aniadirDefinicion(string def);
+
+  typedef vector<string>::iterator iterator;
+  typedef vector<string>::const_iterator const_iterator;
+  Termino::iterator begin();
+  Termino::const_iterator begin() const;
+  Termino::iterator end();
+  Termino::const_iterator end() const;
+
+  friend ostream& operator<< (ostream & os, const Termino & t);
+  friend istream& operator>> (istream & is, Termino & t);
+}
