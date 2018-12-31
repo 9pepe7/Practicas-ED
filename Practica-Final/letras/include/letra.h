7194fec11cc83eb8adb7a7d4f25c35b2090ad7dd
9pepe7 <pepenc97@hotmail.com>
2018-12-31 14:39:14 +0100

    cpp base para jugar a letras

---
+++ Practica-Final/letras/include/letra.h
@@ -0,0 +1,18 @@
+#ifndef LETRA_H
+#define LETRA_H
+
+#include <set>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class letra{
+private:
+  char caracter;
+  int cantidad;
+  int puntuacion;
+public:
+  
+};
+#endif
---
+++ Practica-Final/letras/src/letras.cpp
@@ -0,0 +1,42 @@
+/* Este programa construye palabras de longitud mayor (o de puntuación mayor) a partir de una
+serie de letras seleccionadas de forma aleatoria. El programa letras se deberá ejecutar en la
+línea de órdenes de la siguiente manera:
+prompt% letras spanish.txt letras.txt 8 L*/
+#include <fstream>
+#include <iostream>
+using namespace std;
+int main(int argc, char * argv[]){
+  if (argc!=5){
+    cout<<"Los parametros son:"<<endl;
+    cout << "1. El nombre del fichero con lista_palabras." << endl;
+    cout << "2. El nombre del fichero con las letras." << endl;
+    cout << "3. El número de letras que se deben generar de forma aleatoria." << endl;
+    cout << "4. Modalidad de juego:" << endl;
+    cout << "• Longitud: Si el parámetro es L se buscará la palabra más larga." << endl;
+    cout << "• Puntuación: Si el parámetro es P se buscará la palabra de mayor puntuación." << endl;
+    return 0;
+  }
+  ifstream f_lista_palabras(argv[1]);
+  ifstream f_letras(argv[2]);
+  if (!f_lista_palabras){
+    cout<<"No puedo abrir el fichero"<<argv[1]<<endl;
+    return 0;
+  }
+  if (!f_letras){
+    cout<<"No puedo abrir el fichero"<<argv[2]<<endl;
+    return 0;
+  }
+  if(stoi(argv[3])<1){
+    cout << "Debe generarse al menos una letra." << endl;
+    return 0;
+  }
+  if( argv[4][0]!='L' && argv[4][0]!='P' ){
+    cout << "Debe seleccionarse una modalidad de juego valida (L o P)." << endl;
+    return 0;
+  }
+  char juega_otra_vez;
+  do{
+    cout << "¿Quieres seguir jugando[S/N]?";
+    cin >> juega_otra_vez;
+  } while (juega_otra_vez == 'S')
+}
